89ec319c79e7b652af8c1f831fabbe48501b557c
Luis Azalde Leyva <luis.azalde18@gmail.com>
2022-11-24 03:56:08 -0500

    initial_project

---
+++ app/Http/Controllers/ProductosController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Producto;
+use Illuminate\Http\Request;
+
+class ProductosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view("productos.productos_index", ["productos" => Producto::all()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("productos.productos_create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $producto = new Producto($request->input());
+        $producto->saveOrFail();
+        return redirect()->route("productos.index")->with("mensaje", "Producto guardado");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Producto $producto)
+    {
+        return view("productos.productos_edit", ["producto" => $producto,]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Producto $producto)
+    {
+        $producto->fill($request->input());
+        $producto->saveOrFail();
+        return redirect()->route("productos.index")->with("mensaje", "Producto actualizado");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Producto $producto)
+    {
+        $producto->delete();
+        return redirect()->route("productos.index")->with("mensaje", "Producto eliminado");
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Http\Controllers\ClientesController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductosController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VenderController;
+use App\Http\Controllers\VentasController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect()->route('home');
+});
+
+Route::get("/acerca-de", function () {
+    return view("misc.acerca_de");
+})->name("acerca_de.index");
+
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+// Permitir logout con petición get
+Route::get("/logout", function () {
+    Auth::logout();
+    return redirect()->route("home");
+})->name("logout");
+
+Route::middleware("auth")
+    ->group(function () {
+        Route::resource("clientes", ClientesController::class);
+        Route::resource("usuarios", UserController::class)->parameters(["usuarios" => "user"]);
+        Route::resource("productos", ProductosController::class);
+        Route::get("/ventas/ticket", [VentasController::class, 'ticket'])->name("ventas.ticket");
+        Route::resource("ventas", VentasController::class);
+        Route::get("/vender", [VenderController::class, 'index'])->name("vender.index");
+        Route::post("/productoDeVenta", [VenderController::class, 'agregarProductoVenta'])->name("agregarProductoVenta");
+        Route::delete("/productoDeVenta", [VenderController::class, 'quitarProductoDeVenta'])->name("quitarProductoDeVenta");
+        Route::post("/terminarOCancelarVenta", [VenderController::class, 'terminarOCancelarVenta'])->name("terminarOCancelarVenta");
+    });
+
+    Route::middleware([
+        'auth:sanctum',
+        config('jetstream.auth_session'),
+        'verified'
+    ])->group(function () {
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->name('dashboard');
+    });
